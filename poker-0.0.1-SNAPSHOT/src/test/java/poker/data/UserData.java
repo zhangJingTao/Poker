c1492cbe11151e03f7ae3a09a153eaff3c03923e
zhangJingtao <610039879@qq.com>
2013-12-10 14:27:32 +0800

    new version base on springside next will push redis,flat UI...

---
+++ poker-0.0.1-SNAPSHOT/src/test/java/poker/data/UserData.java
@@ -0,0 +1,16 @@
+package poker.data;
+
+import poker.entity.User;
+import org.springside.modules.test.data.RandomData;
+
+public class UserData {
+
+	public static User randomNewUser() {
+		User user = new User();
+		user.setLoginName(RandomData.randomName("user"));
+		user.setName(RandomData.randomName("User"));
+		user.setPlainPassword(RandomData.randomName("password"));
+
+		return user;
+	}
+}

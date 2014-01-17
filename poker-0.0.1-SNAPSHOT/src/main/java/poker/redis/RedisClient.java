c974b3f07ba79e13a7d281e694f1b59f31b1b2f0
zhangJingtao <610039879@qq.com>
2014-01-17 17:09:49 +0800

    redis客户端

---
+++ poker-0.0.1-SNAPSHOT/src/main/java/poker/redis/RedisClient.java
@@ -0,0 +1,29 @@
+package poker.redis;
+
+import java.util.ResourceBundle;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisClient {
+	private static JedisPool pool;
+	static {
+		ResourceBundle bundle = ResourceBundle.getBundle("redis");
+		if (bundle == null) {
+			throw new IllegalArgumentException(
+					"[redis.properties] is not found!");
+		}
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxActive(Integer.valueOf(bundle
+				.getString("redis.pool.maxActive")));
+		config.setMaxIdle(Integer.valueOf(bundle
+				.getString("redis.pool.maxIdle")));
+		config.setMaxWait(Long.valueOf(bundle.getString("redis.pool.maxWait")));
+		config.setTestOnBorrow(Boolean.valueOf(bundle
+				.getString("redis.pool.testOnBorrow")));
+		config.setTestOnReturn(Boolean.valueOf(bundle
+				.getString("redis.pool.testOnReturn")));
+		pool = new JedisPool(config, bundle.getString("redis.ip"),
+				Integer.valueOf(bundle.getString("redis.port")));
+	}
+}
